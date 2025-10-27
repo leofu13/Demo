38992424eefa5660494ef090c4ebbf98b3435c5f
Leo Fu <leofu@Leos-MacBook-Air-3.local>
2025-10-27 13:06:28 -0700

    Added Mean of Score

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
 data = {'Name': ['Arabica', 'Robusta', 'Liberica'],
-        'Origin': ['SouthAmerica', 'Africa', 'SEA']
+        'Origin': ['SouthAmerica', 'Africa', 'SEA'],
+        'Score': [8, 7, 9] 
         }
 
 df = pd.DataFrame(data)
+Score = df['Score'].mean()
 
+print("MeanScore:", Score)
 print(df)
+
